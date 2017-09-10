d68828dbe0e21ef1024aebb5e8c29a96fe9bab14
Justin Tanacredi <jtanacredi@gmail.com>
2017-09-10 16:10:04 -0400

    clean up printResults method formatting and concatenated string. add check for argument length

--- FundCalculator.rb
+++ FundCalculator.rb
@@ -7,26 +7,23 @@ class FundCalculator
         line.strip
       end
       @company = fundTextLines[0]
+      # Make sure year is length of 4
       @year = fundTextLines[1]
+      # Make sure months formatting is proper
       @months = fundTextLines[2].split(',').map { |month| month.to_f  }
     end
   end
 
-  # returns cumulative return
   def calculateCumulativeReturn
     sprintf("%.2f", @months.reduce(0, :+))
   end
 
-  # returns % postive months
   def calculatePositiveMonths
-    # find all positive months
     positiveMonths = @months.select do |month|
       month > 0
     end
-
     #fix for now without BigDecimal
     sprintf("%.5s", (positiveMonths.length.to_f / @months.length.to_f) * 100).to_f
-
     # percentPositive = (positiveMonths.length.to_f / @months.length.to_f) * 100
     # truncatedPercentPositive = BigDecimal::new(percentPositive.to_s).truncate(2).to_f
     # puts truncatedPercentPositive
@@ -38,14 +35,17 @@ class FundCalculator
   end
 
   def printResults
-    puts "#{@company}\nYear: #{@year}\nCumulative Return: #{self.calculateCumulativeReturn}\n% Positive Months: #{self.calculatePositiveMonths}\nBest Month: #{self.findBestMonth}"
+    puts "#{@company}\nYear: #{@year}\nCumulative Return: #{self.calculateCumulativeReturn}%\n" +
+    "% Positive Months: #{self.calculatePositiveMonths}%\nBest Month: #{self.findBestMonth}%"
   end
 
 end
 
+# Move all terminal functionality outside of FundCalculator file
 ARGV.each do |path|
-  # throwError here if more than 1 argument passed through terminal
-
+  if ARGV.length > 1
+    raise RuntimeError.new('Only 1 argument allowed!')
+  end
 
   fund = FundCalculator.new(path)
   fund.printResults
