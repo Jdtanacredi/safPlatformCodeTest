d97c29ae71215c2388b32aa7ca02607b50310410
Justin Tanacredi <jtanacredi@gmail.com>
2017-09-10 15:58:26 -0400

    Fix decimal placements for FundCalculator methods. create print method for FundCalculator

--- FundCalculator.rb
+++ FundCalculator.rb
@@ -4,7 +4,7 @@ class FundCalculator
   def initialize(path)
     File.open(path) do |f|
       fundTextLines = f.map do |line|
-        line
+        line.strip
       end
       @company = fundTextLines[0]
       @year = fundTextLines[1]
@@ -14,7 +14,7 @@ class FundCalculator
 
   # returns cumulative return
   def calculateCumulativeReturn
-    @months.reduce(0, :+)
+    sprintf("%.2f", @months.reduce(0, :+))
   end
 
   # returns % postive months
@@ -24,7 +24,7 @@ class FundCalculator
       month > 0
     end
 
-    # hacky fix for now without BigDecimal
+    #fix for now without BigDecimal
     sprintf("%.5s", (positiveMonths.length.to_f / @months.length.to_f) * 100).to_f
 
     # percentPositive = (positiveMonths.length.to_f / @months.length.to_f) * 100
@@ -34,7 +34,11 @@ class FundCalculator
 
   # returns best performing month
   def findBestMonth
-    @months.max
+    sprintf("%.2f", @months.max)
+  end
+
+  def printResults
+    puts "#{@company}\nYear: #{@year}\nCumulative Return: #{self.calculateCumulativeReturn}\n% Positive Months: #{self.calculatePositiveMonths}\nBest Month: #{self.findBestMonth}"
   end
 
 end
@@ -42,16 +46,7 @@ end
 ARGV.each do |path|
   # throwError here if more than 1 argument passed through terminal
 
+
   fund = FundCalculator.new(path)
-
-  # TODO move into FundCalculator as a function
-  # puts 'Cumulative Return: ' + fund.calculateCumulativeReturn
-  # puts '% Positive Months: ' + fund.calculatePositiveMonths
-  # puts 'Best Month: ' + fund.findBestMonth
+  fund.printResults
 end
-
-# Bloomberg Barclays US Aggregate Bond TR USD
-# Year: 2016
-# Cumulative Return: 2.66%
-# % Positive Months: 66.66%
-# Best Month: 1.80%
